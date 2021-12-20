91df9d9e0251eba389b6a860710658146d0fc97d
Marcovdw <23501002+Marcovdw@users.noreply.github.com>
2021-12-20 11:24:31 +0100

    Add reset button

--- FavSorterC#/FavSorter/Form.cs
+++ FavSorterC#/FavSorter/Form.cs
@@ -10,19 +10,19 @@ namespace WinFormsApp1
         private string[] elements;
 
         // Bounds of sublists in input
-        private int bll, blu, brl, bru;
+        private int b1l, b1u, b2l, b2u;
 
         // Counters for traversing sublists
-        private int cl, cr;
+        private int c1, c2;
 
         // Temporary array to sort result of submerge
-        private string[] temp = new string[0];
+        private string[] temp;
 
         // Array holding the bounds of sublists to be merged
         private Tuple<int, int>[] rounds;
 
         // Current round
-        private int round = -1;
+        private int round;
 
         // File to write result back towards
         private string fileName;
@@ -34,35 +34,46 @@ namespace WinFormsApp1
 
         private void Next_round()
         {
+            // Write result of submerge back to input
+            for (int i = 0; i < temp.Length; i++)
+                elements[b1l + i] = temp[i];
+
             // Terminate if all rounds are performed
             if (++round == rounds.Length)
             {
+                // Write and show results
                 System.IO.File.WriteAllLines(fileName, elements);
-                button1.Enabled = false;
-                button2.Enabled = false;
-                label1.Text = "Done";
                 Process.Start("notepad.exe", fileName);
+
+                // Reset buttons and lables
+                button1.Enabled = false;
+                button1.Text = "";
+                button2.Enabled = false;
+                button2.Text = "";
+                button3.Enabled = true;
+                button3.Text = "Load file";
+                label1.Text = "Done";
             }
             else
             {
-                // Write result of submerge back to input
-                for (int i = 0; i < temp.Length; i++)
-                    elements[bll + i] = temp[i];
                 // Reset counters
-                cl = 0;
-                cr = 0;
+                c1 = 0;
+                c2 = 0;
+
                 // Reset bounds
                 Tuple<int, int> t = rounds[round];
                 int h = (t.Item2 - t.Item1) / 2;
-                bll = t.Item1;
-                blu = t.Item1 + h;
-                brl = t.Item1 + h + 1;
-                bru = t.Item2;
+                b1l = t.Item1;
+                b1u = t.Item1 + h;
+                b2l = t.Item1 + h + 1;
+                b2u = t.Item2;
+
                 // Clear temp
                 temp = new string[t.Item2 - t.Item1 + 1];
+
                 // Set first matchup
-                button1.Text = elements[bll];
-                button2.Text = elements[brl];
+                button1.Text = elements[b1l];
+                button2.Text = elements[b2l];
             }
         }
 
@@ -76,16 +87,22 @@ namespace WinFormsApp1
             };
             if (openFileDialog.ShowDialog() == DialogResult.OK)
             {
+                // Read input and store to elements
                 fileName = openFileDialog.FileName;
-                button3.Enabled = false;
+                elements = System.IO.File.ReadAllLines(fileName);
+
+                // Set buttons, labels and variables
                 button1.Enabled = true;
                 button2.Enabled = true;
+                button3.Enabled = false;
                 label1.Text = "Versus";
-                // Read input and store to elements
-                elements = System.IO.File.ReadAllLines(fileName);
+                temp = new string[0];
+                round = -1;
+
                 // Set rounds array
                 rounds = new Tuple<int, int>[elements.Length - 1];
                 Set_rounds(0, elements.Length - 1, 0, elements.Length - 2);
+
                 // Begin
                 Next_round();
             }
@@ -93,36 +110,46 @@ namespace WinFormsApp1
 
         private void Button1_Click(object sender, EventArgs e)
         {
-            // Write left element to temp array
-            temp[cl + cr] = elements[bll + cl++];
-            if (bll + cl > blu)
-            {
-                // Left sublist is exhausted, add remaining right elements in order
-                for (int i = brl + cr; i <= bru; i++)
-                    temp[cl + cr++] = elements[i];
-                // Go to next round
-                Next_round();
-            }
-            else
-                // Next matchup
-                button1.Text = elements[bll + cl];
+            match(ref button1, ref c1, ref c2);
         }
 
         private void Button2_Click(object sender, EventArgs e)
         {
-            // Write right element to temp array
-            temp[cl + cr] = elements[brl + cr++];
-            if (brl + cr > bru)
+            match(ref button2, ref c2, ref c1);
+        }
+
+        private void match(ref Button button, ref int cw, ref int cl)
+        {
+            // Set bounds
+            int bwl, bwu, bll, blu;
+            if(button == button1)
             {
-                // Right sublist is exhausted, add remaining right elements in order
+                bwl = b1l;
+                bwu = b1u;
+                bll = b2l;
+                blu = b2u;
+            }
+            else
+            {
+                bwl = b2l;
+                bwu = b2u;
+                bll = b1l;
+                blu = b1u;
+            }
+
+            // Write winning element to temp array
+            temp[cw + cl] = elements[bwl + cw++];
+            if (bwl + cw > bwu)
+            {
+                // A sublist is exhausted, add remaining elements in order
                 for (int i = bll + cl; i <= blu; i++)
-                    temp[cl++ + cr] = elements[i];
+                    temp[cw + cl++] = elements[i];
                 // Go to next round
                 Next_round();
             }
             else
                 // Next matchup
-                button2.Text = elements[brl + cr];
+                button.Text = elements[bwl + cw];
         }
 
         private void Set_rounds(int el, int eu, int rl, int ru)
